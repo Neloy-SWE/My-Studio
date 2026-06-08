28f8f7574d1113bb40c1d43c0478d744a9ac1b84
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-06-08 12:51:42 +0600

    project card mobile view added

--- src/component/Card/CardProject.jsx
+++ src/component/Card/CardProject.jsx
@@ -11,15 +11,15 @@ const CardProject = (
 ) => {
     return (
         <>
-            <div onClick={() => openURL(url)} className="group w-[300px] bg-bg xl:mx-4 mx-[0px] my-4 xl:my-[0px]  border-[0.5px] border-transparent hover:border-white rounded-[10px] cursor-pointer transition duration-300">
+            <div onClick={() => openURL(url)} className="group lg:w-[300px] bg-bg xl:mx-4 mx-[0px] my-4 xl:my-[0px]  border-[0.5px] border-transparent hover:border-white rounded-[10px] cursor-pointer transition duration-300">
                 <img src={bannerPath} alt={bannerPath} className="w-full rounded-t-[10px] object-cover grayscale brightness-75 group-hover:grayscale-0 group-hover:brightness-100 transition duration-500 ease-in-out" />
                 <div className="px-6 py-8">
-                    <h1 className="text-white text-bold text-[20px] mb-[25px]">{title}</h1>
-                    <p className="text-ts text-[14px] mb-[35px]">{description}</p>
+                    <h1 className="text-white text-bold text-[16px] lg:text-[20px] mb-[25px]">{title}</h1>
+                    <p className="text-ts text-[12px] lg:text-[14px] mb-[35px]">{description}</p>
 
                     <div className="flex flex-wrap gap-4">{
                         stackList.map((value, index) => (
-                            <h3 key={index} className="text-primary text-[12px]">{value}</h3>
+                            <h3 key={index} className="text-primary text-[10px] lg:text-[12px]">{value}</h3>
                         ))
                     }</div>
                 </div>
